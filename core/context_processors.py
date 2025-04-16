af080edea689407c859b23f15dae3a6b6e4afc5c
peter-njuku <137099178+peter-njuku@users.noreply.github.com>
2025-04-16 16:49:58 +0300

    Update context_processors.py

--- core/context_processors.py
+++ core/context_processors.py
@@ -4,3 +4,6 @@ def current_year():
     return {
         'current_year':datetime.datetime.now().year
     }
+# context_processors.py
+def device_type(request):
+    return {'device_type': getattr(request, 'device_type', 'unknown')}
