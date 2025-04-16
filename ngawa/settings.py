ebf35910d6984f666077d6fd1de13fe23bcbd113
peter-njuku <137099178+peter-njuku@users.noreply.github.com>
2025-04-16 16:51:29 +0300

    Update settings.py

--- ngawa/settings.py
+++ ngawa/settings.py
@@ -50,7 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'middleware.device_middleware.DeviceDetectionMiddleware',
+    'device_middleware.DeviceDetectionMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
